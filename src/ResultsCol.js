43bb2ff7b1c6fc09238010a5a76477f9dca1507d
Jack Broomer <jbroomer@umass.edu>
2019-05-31 21:54:36 -0400

    Click TV image to redirect to website

--- src/ResultsCol.js
+++ src/ResultsCol.js
@@ -8,6 +8,7 @@ class ResultsCol extends React.Component{
     this.state = {open: false};
 
     this.onClick = this.onClick.bind(this);
+    this.visitShowPage = this.visitShowPage.bind(this);
   }
 
   onClick(id){
@@ -19,13 +20,18 @@ class ResultsCol extends React.Component{
     });
     this.setState({buttons: stateArray});
   }
+
+  visitShowPage(){
+    const newPage = "https://www.themoviedb.org/tv/" + this.props.episode.id + "?language=en-US"
+    window.open(newPage);
+  }
   
   
   render(){
       const tvIdString = (this.props.episode.id).toString();
         return <Col xs={4} style = {{paddingBottom: '10px'}}>
           <Card>
-          <Card.Img style = {{width: '100%', height: '400px'}} variant="top" src = {this.props.episode.tvImg}/>
+          <Card.Img onClick = {this.visitShowPage} style = {{width: '100%', height: '400px'}} variant="top" src = {this.props.episode.tvImg}/>
           <Card.Body>
             <Card.Title>{this.props.episode.original_name} ({this.props.episode.year})</Card.Title>
             {/* <Card.Text>{this.props.episode.overview}</Card.Text> */}
