ce1602ccdeb5954068a155bf8b0bbb9d84b2d7f0
Jack Broomer <jbroomer@umass.edu>
2019-06-12 22:22:58 -0400

    Fixed excess calls to API and removed dead code

--- src/RandomGen.js
+++ src/RandomGen.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import $ from 'jquery';
+import {Button, Collapse} from 'react-bootstrap';
 
 class RandomGen extends React.Component{
     constructor(props){
         super(props);
         
-        this.state = {season: 1, episode: 1};
-        
-        this.getSeasonDetails();
-        this.getEpisodeDetails();
+        this.state = {season: 1, episode: 1, open: false};
     }
+    //Changes the state to open to display the generated episode
+    onClick(id){
+        let stateArray = this.state.buttons;
+        stateArray.forEach((button) => {
+          if(button.id === this.props.episode.id){
+            button.open = true;
+          }
+        });
+        this.setState({buttons: stateArray});
+      }
 
-
-    
     //Retrieves the number of seasons in the selected episode
     getSeasonDetails(){
         const urlString = "https://api.themoviedb.org/3/tv/" + this.props.buttonId + "?api_key=6e8556079c0e1a842e60fdb88680228f";
         $.ajax({
           url: urlString,
           success: (searchResults) => {
+            console.log("Searching...2");
             const numSeason = (searchResults.number_of_seasons);
             const randomSeason = Math.floor(Math.random() * (+(numSeason+1) - +1)) + + 1;
           
@@ -38,6 +45,7 @@ class RandomGen extends React.Component{
         $.ajax({
           url: urlString,
           success: (searchResults) => {
+            console.log("Searching...3");
             const numEpisodes = (searchResults.episodes.length);
             randomEpisode = Math.floor(Math.random() * (+(numEpisodes+1) - +1)) + + 1;
             this.setState({episode: randomEpisode});
@@ -45,30 +53,36 @@ class RandomGen extends React.Component{
           error: (xhr, status, err) => {
             console.log(err);
           }
-
         });
-        
     }
 
 
-
-    render(){
-        
+    render(){ 
         return (
-        
         <div style = {{textAlign: 'center'}}>
             
-            Season: {this.state.season} Episode: {this.state.episode}
+            <Button 
+                id = {this.props.buttonId} 
+                variant="outline-secondary" 
+                onClick = {() => {this.setState({open: !this.state.open}); this.getSeasonDetails(); this.getEpisodeDetails();}}
+                style = {{width: '100%', marginLeft: 'auto', marginRight:'auto', marginBottom: '10px'
+                }}>
+                    Random Episode
+            </Button>
+            
+            <Collapse id = {this.props.buttonId} in={this.state.open} >
+                <div>
+                    Season: {this.state.season} Episode: {this.state.episode}
 
-            <img alt = 'refresh' src = './refresh.png' style = {{width: '25px', marginLeft: '20px'}} onClick = {() => {
-                this.getSeasonDetails();
-                this.getEpisodeDetails();
-            }}/>
-
+                    <img alt = 'refresh' src = './refresh.png' style = {{width: '25px', marginLeft: '20px'}} onClick = {() => {
+                        this.getSeasonDetails();
+                        this.getEpisodeDetails();
+                    }}/>
+                </div>
+            </Collapse>
         </div>
         )
     }
-
 }
 
 export default RandomGen;
