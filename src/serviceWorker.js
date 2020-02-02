92d5c5e95be9e0e750678f6e4a6a4c185de4592f
Jack Broomer <jbroomer@umass.edu>
2020-02-02 15:17:55 -0500

    Debugging sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -22,6 +22,7 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    console.log("Made it this far");
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
